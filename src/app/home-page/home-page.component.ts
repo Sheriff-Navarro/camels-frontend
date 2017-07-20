3a07db51bb7afa67b43b9702f0ef567aa79d1a14
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-07-20 18:24:36 -0400

    Add sign up form

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthServiceService } from '../services/auth-service.service';
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  fullNameValue: string;
+  emailValue: string;
+  passwordValue: string;
+
+  constructor(
+    private authThang: AuthServiceService
+  ) { }
 
   ngOnInit() {
+  }
+
+  doSignUp() {
+    this.authThang.signup(this.fullNameValue, this.emailValue, this.passwordValue)
+      .then((resultFromApi) => {
+          alert('Sign up worked! ' + resultFromApi._id);
+          console.log(resultFromApi);
+      })
+      .catch((err) => {
+          alert('Error 😤');
+          console.log(err);
+      });
   }
 
 }
